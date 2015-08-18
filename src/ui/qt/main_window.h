1a1a2509e4df9d333edc9bc1f3786034c2807f1d
Daniel Andrus <dan.andrus@7400circuits.com>
2015-08-18 15:58:02 -0600

    + Added game system selection box + Buttons automatically disable/enable based on game system selection + Hooked up ngp and ws functionality to ui

--- src/ui/qt/main_window.h
+++ src/ui/qt/main_window.h
@@ -2,6 +2,7 @@
 #define __MAINWINDOW_H__
 
 #include <QMainWindow>
+#include "cartridge/cartridge.h"
 
 namespace Ui {
 class MainWindow;
@@ -18,6 +19,7 @@ public:
   void print_to_console(const char* message, ...);
   
 private slots:
+  void on_combobox_system_type_currentIndexChanged(int index);
   void on_button_backup_rom_clicked();
   void on_button_verify_rom_clicked();
   void on_button_flash_rom_clicked();
@@ -25,7 +27,21 @@ private slots:
   void on_button_restore_save_clicked();
   
 private:
+  enum button_flags {
+    ROM_FLASH = 1 << 0,
+    ROM_BACKUP = 1 << 1,
+    ROM_VERIFY = 1 << 2,
+    SAVE_RESTORE = 1 << 3,
+    SAVE_BACKUP = 1 << 4,
+    CARTRIDGE_INFO = 1 << 5,
+    PREFERENCES = 1 << 6
+  };
+  
+  void enable_buttons(int buttons);
+  
   Ui::MainWindow *ui;
+  system_type m_target_system;
+  
 };
 
 #endif // __MAINWINDOW_H__
