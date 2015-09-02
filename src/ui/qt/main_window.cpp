aa734af13d925f809521c7c2267154b9d40718bd
Daniel Andrus <dan.andrus@7400circuits.com>
2015-09-02 13:27:25 -0600

    * Redesigned UI

--- src/ui/qt/main_window.cpp
+++ src/ui/qt/main_window.cpp
@@ -21,15 +21,8 @@ MainWindow::MainWindow(QWidget *parent)
 {
   ui->setupUi(this);
   
-  const QFont fixedFont = QFontDatabase::systemFont(QFontDatabase::FixedFont);
-  ui->textBrowser->setFont(fixedFont);
-  ui->combobox_system_type->setCurrentIndex(0);
-  
-  print_to_console("");
-  print_to_console("-=> NeoLinkMasta GUI v%d.%d build %d <=-", 1, 2, 1001);
-  print_to_console("");
-  print_to_console("     (c) Mr.Spiv & Flavor in 2011.");
-  print_to_console("");
+  ui->deviceListWidget->addItem("Neo Geo Pocket Linkmasta");
+  ui->deviceListWidget->addItem("WonderSwan FlashMasta");
 }
 
 MainWindow::~MainWindow()
@@ -39,136 +32,72 @@ MainWindow::~MainWindow()
 
 
 
-void MainWindow::print_to_console(const char* message, ...)
+void MainWindow::on_actionBackupROM_triggered()
 {
-  char buf[1024];
-  va_list args;
-  
-  // Format string and arguments and store in buffer
-  va_start(args, message);
-  vsprintf(buf, message, args);
-  
-  // Append message to console
-  ui->textBrowser->append(buf);
-  
-  va_end(args);
-}
-
-
-
-void MainWindow::on_combobox_system_type_currentIndexChanged(int index)
-{
-  switch (index)
+  switch (ui->deviceListWidget->currentRow())
   {
   case 0:
-    enable_buttons(ROM_FLASH | ROM_BACKUP | ROM_VERIFY | SAVE_RESTORE | SAVE_BACKUP);
-    m_target_system = system_type::NEO_GEO_POCKET;
-    break;
-    
-  case 1:
-    enable_buttons(ROM_FLASH | ROM_BACKUP | ROM_VERIFY | SAVE_RESTORE | SAVE_BACKUP);
-    m_target_system = system_type::WONDERSWAN;
-    break;
-    
-  default:
-    enable_buttons(0);
-    m_target_system = system_type::UNKNOWN;
-    break;
-  }
-}
-
-void MainWindow::on_button_backup_rom_clicked()
-{
-  switch (m_target_system)
-  {
-  case NEO_GEO_POCKET:
     NgpCartridgeBackupTask(this).go();
     break;
     
-  case WONDERSWAN:    
+  case 1:
     WsCartridgeBackupTask(this).go();
     break;
-    
-  default:
-    break;
   }
 }
 
-void MainWindow::on_button_verify_rom_clicked()
+void MainWindow::on_actionRestoreROM_triggered()
 {
-  switch (m_target_system)
+  switch (ui->deviceListWidget->currentRow())
   {
-  case NEO_GEO_POCKET:
-    NgpCartridgeVerifyTask(this).go();
-    break;
-    
-  case WONDERSWAN:
-    WsCartridgeVerifyTask(this).go();
-    break;
-    
-  default:
-    break;
-  }
-}
-
-void MainWindow::on_button_flash_rom_clicked()
-{
-  switch (m_target_system)
-  {
-  case NEO_GEO_POCKET:
+  case 0:
     NgpCartridgeFlashTask(this).go();
     break;
     
-  case WONDERSWAN:
+  case 1:
     WsCartridgeFlashTask(this).go();
-    break;
-    
-  default:
     break;
   }
 }
 
-void MainWindow::on_button_backup_save_clicked()
+void MainWindow::on_actionVerifyROM_triggered()
 {
-  switch (m_target_system)
+  switch (ui->deviceListWidget->currentRow())
   {
-  case NEO_GEO_POCKET:
+  case 0:
+    NgpCartridgeVerifyTask(this).go();
+    break;
+    
+  case 1:
+    WsCartridgeVerifyTask(this).go();
+    break;
+  }
+}
+
+void MainWindow::on_actionBackupSave_triggered()
+{
+  switch (ui->deviceListWidget->currentRow())
+  {
+  case 0:
     NgpCartridgeBackupSaveTask(this).go();
     break;
     
-  default:
+  case 1:
     WsCartridgeBackupSaveTask(this).go();
     break;
   }
 }
 
-void MainWindow::on_button_restore_save_clicked()
+void MainWindow::on_actionRestoreSave_triggered()
 {
-  switch (m_target_system)
+  switch (ui->deviceListWidget->currentRow())
   {
-  case NEO_GEO_POCKET:
+  case 0:
     NgpCartridgeRestoreSaveTask(this).go();
     break;
     
-  default:
+  case 1:
     WsCartridgeRestoreSaveTask(this).go();
     break;
   }
 }
-
-
-
-void MainWindow::enable_buttons(int buttons)
-{
-  Ui::MainWindow* ui= this->ui;
-  
-  ui->button_flash_rom->setEnabled((buttons & button_flags::ROM_FLASH) != 0);
-  ui->button_backup_rom->setEnabled((buttons & button_flags::ROM_BACKUP) != 0);
-  ui->button_verify_rom->setEnabled((buttons & button_flags::ROM_VERIFY) != 0);
-  ui->button_restore_save->setEnabled((buttons & button_flags::SAVE_RESTORE) != 0);
-  ui->button_backup_save->setEnabled((buttons & button_flags::SAVE_BACKUP) != 0);
-  ui->button_cartridge_info->setEnabled((buttons & button_flags::CARTRIDGE_INFO) != 0);
-  ui->button_preferences->setEnabled((buttons & button_flags::PREFERENCES) != 0);
-}
-
-
--- src/ui/qt/main_window.h
+++ src/ui/qt/main_window.h
@@ -16,29 +16,17 @@ public:
   explicit MainWindow(QWidget *parent = 0);
   ~MainWindow();
   
-  void print_to_console(const char* message, ...);
-  
 private slots:
-  void on_combobox_system_type_currentIndexChanged(int index);
-  void on_button_backup_rom_clicked();
-  void on_button_verify_rom_clicked();
-  void on_button_flash_rom_clicked();
-  void on_button_backup_save_clicked();
-  void on_button_restore_save_clicked();
+  void on_actionBackupROM_triggered();
+  void on_actionRestoreROM_triggered();
+  
+  void on_actionVerifyROM_triggered();
+  
+  void on_actionBackupSave_triggered();
+  
+  void on_actionRestoreSave_triggered();
   
 private:
-  enum button_flags {
-    ROM_FLASH = 1 << 0,
-    ROM_BACKUP = 1 << 1,
-    ROM_VERIFY = 1 << 2,
-    SAVE_RESTORE = 1 << 3,
-    SAVE_BACKUP = 1 << 4,
-    CARTRIDGE_INFO = 1 << 5,
-    PREFERENCES = 1 << 6
-  };
-  
-  void enable_buttons(int buttons);
-  
   Ui::MainWindow *ui;
   system_type m_target_system;
   
