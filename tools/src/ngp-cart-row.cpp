ec23bec97d9867bd836b7995cc3b303055b07dee
Daniel Andrus <dan.andrus@7400circuits.com>
2015-12-22 08:22:32 -0700

    Parameter binding added to ngp_cart_row

--- tools/src/ngp-cart-row.cpp
+++ tools/src/ngp-cart-row.cpp
@@ -56,9 +56,118 @@ bool ngp_cart_row::parse_xml(const node_t* node)
 
 bool ngp_cart_row::bind_to_stmt(sqlite3_stmt* stmt)
 {
-  // TODO
-  (void) stmt;
-  return false;
+  int ind;
+  string param;
+  
+  ind = sqlite3_bind_parameter_index(stmt, ":cartchips");
+  if (ind == 0)
+  {
+    cerr << "Unable to find index of parameter 'cartchips'" << endl;
+    return false;
+  }
+  if (sqlite3_bind_int(stmt, ind, CartChips) != SQLITE_OK)
+  {
+    cerr << "Unable to bind parameter 'cartchips'" << endl;
+    return false;
+  }
+  
+  ind = sqlite3_bind_parameter_index(stmt, ":cartsize");
+  if (ind == 0)
+  {
+    cerr << "Unable to find index of parameter 'cartsize'" << endl;
+    return false;
+  }
+  if (sqlite3_bind_int(stmt, ind, CartSize) != SQLITE_OK)
+  {
+    cerr << "Unable to bind parameter 'cartsize'" << endl;
+    return false;
+  }
+  
+  ind = sqlite3_bind_parameter_index(stmt, ":gameid");
+  if (ind == 0)
+  {
+    cerr << "Unable to find index of parameter 'gameid'" << endl;
+    return false;
+  }
+  if (sqlite3_bind_int(stmt, ind, GameId) != SQLITE_OK)
+  {
+    cerr << "Unable to bind parameter 'gameid'" << endl;
+    return false;
+  }
+  
+  for (int i = 0; i < SaveBlocks.size(); i++)
+  {
+    param = string(":gameid") + to_string(i);
+    ind = sqlite3_bind_parameter_index(stmt, param.c_str());
+    if (ind == 0)
+    {
+      cerr << "Unable to find index of parameter '" << param << "'" << endl;
+      return false;
+    }
+    if (sqlite3_bind_int(stmt, ind, GameId) != SQLITE_OK)
+    {
+      cerr << "Unable to bind parameter '" << param << "'" << endl;
+      return false;
+    }
+    
+    // block number
+    param = string(":blocknumber") + to_string(i);
+    ind = sqlite3_bind_parameter_index(stmt, param.c_str());
+    if (ind == 0)
+    {
+      cerr << "Unable to find index of parameter '" << param << "'" << endl;
+      return false;
+    }
+    if (sqlite3_bind_int(stmt, ind, SaveBlocks[i].BlockNumber) != SQLITE_OK)
+    {
+      cerr << "Unable to bind parameter '" << param << "'" << endl;
+      return false;
+    }
+    
+    // chip
+    param = string(":chip") + to_string(i);
+    ind = sqlite3_bind_parameter_index(stmt, param.c_str());
+    if (ind == 0)
+    {
+      cerr << "Unable to find index of parameter '" << param << "'" << endl;
+      return false;
+    }
+    if (sqlite3_bind_int(stmt, ind, SaveBlocks[i].Chip) != SQLITE_OK)
+    {
+      cerr << "Unable to bind parameter '" << param << "'" << endl;
+      return false;
+    }
+    
+    // address
+    param = string(":address") + to_string(i);
+    ind = sqlite3_bind_parameter_index(stmt, param.c_str());
+    if (ind == 0)
+    {
+      cerr << "Unable to find index of parameter '" << param << "'" << endl;
+      return false;
+    }
+    if (sqlite3_bind_int(stmt, ind, SaveBlocks[i].Address) != SQLITE_OK)
+    {
+      cerr << "Unable to bind parameter '" << param << "'" << endl;
+      return false;
+    }
+    
+    // size
+    param = string(":size") + to_string(i);
+    ind = sqlite3_bind_parameter_index(stmt, param.c_str());
+    if (ind == 0)
+    {
+      cerr << "Unable to find index of parameter '" << param << "'" << endl;
+      return false;
+    }
+    if (sqlite3_bind_int(stmt, ind, SaveBlocks[i].Size) != SQLITE_OK)
+    {
+      cerr << "Unable to bind parameter '" << param << "'" << endl;
+      return false;
+    }
+  }
+  
+  return true;
 }
 
 }
